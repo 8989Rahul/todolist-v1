4aab98b16e131129acfe1f364e514cd192d3e16c
8989Rahul <daghalerahul0@gmail.com>
2020-04-16 09:51:59 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const today = new Date();
+  let day = "";
+  if (today.getDay() === 0) {
+    day = "Sunday";
+  } else if (today.getDay() === 1) {
+    day = "Monday";
+  } else if (today.getDay() === 2) {
+    day = "Tuesday";
+  } else if (today.getDay() === 3) {
+    day = "Wednesday";
+  } else if (today.getDay() === 4) {
+    day = "Thursday";
+  } else if (today.getDay() === 5) {
+    day = "Friday";
+  } else if (today.getDay() === 6) {
+    day = "Saturday";
+  }
+
+  res.render("list", { kindOfDay: day });
+  //   res.sendFile(__dirname + "/index.html");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("Server is running on port 3000");
+});
