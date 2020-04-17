4f6ecdb904e571d13ca8389f2f9959005e68ac0e
8989Rahul <daghalerahul0@gmail.com>
2020-04-17 23:21:14 +0530

    reduce code

--- app.js
+++ app.js
@@ -6,23 +6,8 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   const today = new Date();
-  let day = "";
-  if (today.getDay() === 0) {
-    day = "Sunday";
-  } else if (today.getDay() === 1) {
-    day = "Monday";
-  } else if (today.getDay() === 2) {
-    day = "Tuesday";
-  } else if (today.getDay() === 3) {
-    day = "Wednesday";
-  } else if (today.getDay() === 4) {
-    day = "Thursday";
-  } else if (today.getDay() === 5) {
-    day = "Friday";
-  } else if (today.getDay() === 6) {
-    day = "Saturday";
-  }
-
+  var options = { weekday: "long", month: "long", day: "numeric" };
+  const day = today.toLocaleDateString("en-US", options);
   res.render("list", { kindOfDay: day });
   //   res.sendFile(__dirname + "/index.html");
 });
