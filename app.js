7659ddb3e9262627cfe0b4835c753be92f8e00df
8989Rahul <daghalerahul0@gmail.com>
2020-04-18 23:47:23 +0530

    looping the value

--- app.js
+++ app.js
@@ -4,12 +4,23 @@ const bodyParser = require("body-parser");
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let items = [];
+
 app.get("/", (req, res) => {
   const today = new Date();
   var options = { weekday: "long", month: "long", day: "numeric" };
   const day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day });
+  res.render("list", { kindOfDay: day, itemList: items });
   //   res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+  // console.log(item);
 });
 
 app.listen(3000, (req, res) => {
