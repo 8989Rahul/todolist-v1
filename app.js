4c50f28880ecc7bc13cd63b37d60fbb7b22566a7
8989Rahul <daghalerahul0@gmail.com>
2020-04-20 23:19:02 +0530

    trying to render list in ejs file

--- app.js
+++ app.js
@@ -2,26 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let items = [];
 
-app.get("/", (req, res) => {
-  const today = new Date();
+app.get("/", function(req, res) {
+  var today = new Date();
   var options = { weekday: "long", month: "long", day: "numeric" };
-  const day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day, itemList: items });
-  //   res.sendFile(__dirname + "/index.html");
+  var day = today.toLocaleDateString("en-US", options);
+  res.render("list", { kindOfDay: day ,itemList: items});
+  // itemList: items
 });
 
-app.post("/", (req, res) => {
-  var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
-  // console.log(item);
-});
+// app.post("/", (req, res) => {
+//   var item = req.body.newItem;
+//   items.push(item);
+//   res.redirect("/");
+// });
 
 app.listen(3000, (req, res) => {
   console.log("Server is running on port 3000");
