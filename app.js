b8412595126c2d31c38b1581efc303071ee3aecd
8989Rahul <daghalerahul0@gmail.com>
2020-04-23 00:25:16 +0530

    css file atached to List.ejs

--- app.js
+++ app.js
@@ -3,26 +3,26 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-var items = ["Buy Food", "Cook Food", "Eat Food"];
+let items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  let today = new Date();
+  let options = { weekday: "long", month: "long", day: "numeric" };
+  let day = today.toLocaleDateString("en-US", options);
 
-app.get("/", function(req, res) {
-  var today = new Date();
-  var options = { weekday: "long", month: "long", day: "numeric" };
-  var day = today.toLocaleDateString("en-US", options);
-  res.render("list", { kindOfDay: day ,itemList: items});
-  // itemList: items
+  res.render("List", { data: { kindOfDay: day, listItems: items } });
 });
 
-// app.post("/", (req, res) => {
-//   var item = req.body.newItem;
-//   items.push(item);
-//   res.redirect("/");
-// });
+app.post("/", (req, res) => {
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
+});
 
 app.listen(3000, (req, res) => {
   console.log("Server is running on port 3000");
