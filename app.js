f9e0251e8e23f10c091ee5981cb6a756bcf255a2
8989Rahul <daghalerahul0@gmail.com>
2020-04-27 23:32:01 +0530

    implementing ejs layout

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
-
+let workItems = [];
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -15,13 +15,32 @@ app.get("/", (req, res) => {
   let options = { weekday: "long", month: "long", day: "numeric" };
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("List", { data: { kindOfDay: day, listItems: items } });
+  res.render("List", { data: { listTitle: day, listItems: items } });
 });
 
 app.post("/", (req, res) => {
+  console.log(req.body);
+
   var item = req.body.newItem;
-  items.push(item);
-  res.redirect("/");
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", (req, res) => {
+  res.render("List", {
+    data: { listTitle: "Work List", listItems: workItems },
+  });
+});
+
+app.post("/work", (req, res) => {
+  var item = req.body.newItem;
+  workItems.push(item);
+  res.redirect("/work");
 });
 
 app.listen(3000, (req, res) => {
